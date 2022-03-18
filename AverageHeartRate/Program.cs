a8799f79df8e736ffc5ccbbe127d268b8ea914d4
Kurt Frank <kufrank81@gmail.com>
2022-03-18 15:11:46 -0500

    change to parallel loop

--- AverageHeartRate/Program.cs
+++ AverageHeartRate/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -11,7 +13,7 @@ namespace AverageHeartRate
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             HttpClient httpClient = new();
@@ -21,22 +23,35 @@ namespace AverageHeartRate
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", myToken);
             string start_date;
             string end_date;
-            List<int> samples = new();
+            //List<int> samples = new();
             DateTime endDate = DateTime.Now;
             double numberDaysBtEndAndStop = 30;
+            Dictionary<string, string> dates = new();
             do
             {
-                startDate = startDate.AddDays(numberDaysBtEndAndStop + 1);
+                startDate = startDate.AddDays(numberDaysBtEndAndStop).AddTicks(1);
                 start_date = startDate.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
                 end_date = startDate.AddDays(numberDaysBtEndAndStop).ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
-                string url = $"https://api.ouraring.com/v2/usercollection/heartrate?start_datetime={start_date}&end_datetime={end_date}";
+                dates.Add(start_date, end_date);
+            } while (DateTime.Parse(end_date) < endDate);
+            ConcurrentBag<int> samples = new();
+            Parallel.ForEach(dates, (date) =>
+            {
+                string url = $"https://api.ouraring.com/v2/usercollection/heartrate?start_datetime={date.Key}&end_datetime={date.Value}";
                 url = url.Replace("+", "-");
-                //var response = await httpClient.GetFromJsonAsync<Data>($"https://api.ouraring.com/v2/usercollection/heartrate?start_datetime=2021-12-01T00:00:00-00:00&end_datetime=2021-12-02T00:00:00-00:00");
-                Console.WriteLine("sending request"); 
-                var response = await httpClient.GetFromJsonAsync<Data>(url);
-                if (response.data.Count > 0)
+                Uri uri = new(url);
+                Console.WriteLine("sending request");
+                HttpRequestMessage request = new() { RequestUri = uri };
+                var response = httpClient.Send(request); //.GetFromJson<Data>(url);
+                response.EnsureSuccessStatusCode();
+                var stream = response.Content.ReadAsStream();
+                var streamReader = new StreamReader(stream, Encoding.UTF8);
+                string content = streamReader.ReadToEnd();
+                Data contentData = JsonSerializer.Deserialize<Data>(content);
+                Console.WriteLine(contentData.data.Count + "   " + date.Key + "   " + date.Value);
+                if (contentData.data.Count > 0)
                 {
-                    foreach (var item in response.data)
+                    foreach (var item in contentData.data)
                     {
                         if (item.bpm > 0)
                         {
@@ -46,7 +61,7 @@ namespace AverageHeartRate
                     }
 
                 }
-            } while (startDate < endDate);
+            });
 
 
             double totalAverage = 0;
@@ -55,9 +70,9 @@ namespace AverageHeartRate
                 totalAverage += item;
             }
 
-            Console.WriteLine($"Kurt's average heart rate over {samples.Count} while awake is {totalAverage/samples.Count}");
+            Console.WriteLine($"Kurt's average heart rate over {samples.Count} while awake is {totalAverage / samples.Count}");
             Console.WriteLine("Samples taken between 11-30-2021 and today.");
-            
+
             Console.ReadLine();
 
         }
